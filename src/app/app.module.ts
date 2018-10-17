7b633e8e3b376783f4adf7a45bc5f22d927dc863
Mohamed Romdhani <mromdhani7@gmail.com>
2018-10-17 06:35:55 +0100

    Commit initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductsFilterPipe } from './pipes/products-filter.pipe';
+import { StarComponent } from './components/shared/star/star.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsListComponent,
+    ProductsFilterPipe,
+    StarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
